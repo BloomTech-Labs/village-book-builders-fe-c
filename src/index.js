490b43b2f69f482acd3e5c85656eb91da61b91b2
yirano <yirano@protonmail.ch>
2020-12-05 11:24:22 -0500

    Set up axiosWithAuth and PrivateRoute component + comment out Okta settings in index.js

--- src/index.js
+++ src/index.js
@@ -9,7 +9,8 @@ import {
 import { Provider } from 'react-redux';
 import { createStore, applyMiddleware } from 'redux';
 import thunk from 'redux-thunk';
-import { Security, LoginCallback, SecureRoute } from '@okta/okta-react';
+// import { Security, LoginCallback, SecureRoute } from '@okta/okta-react'; <-------------- OKTA --------------
+import PrivateRoute from './utils/PrivateRoute';
 
 import 'antd/dist/antd.less';
 
@@ -20,7 +21,7 @@ import { LoginPage } from './components/pages/Login';
 import { HomePage } from './components/pages/Home';
 import { LandingPage } from './components/pages/Landing';
 import { ExampleDataViz } from './components/pages/ExampleDataViz';
-import { config } from './utils/oktaConfig';
+// import { config } from "./utils/oktaConfig" <-------------- OKTA --------------
 import { LoadingComponent } from './components/common';
 
 import reducer from './state/reducers/index';
@@ -43,29 +44,31 @@ function App() {
   // React Router has a nifty useHistory hook we can use at this level to ensure we have security around our routes.
   const history = useHistory();
 
-  const authHandler = () => {
-    // We pass this to our <Security /> component that wraps our routes.
-    // It'll automatically check if userToken is available and push back to login if not :)
-    history.push('/login');
-  };
+  // const authHandler = () => { <-------------- OKTA --------------
+  // 	// We pass this to our <Security /> component that wraps our routes.
+  // 	// It'll automatically check if userToken is available and push back to login if not :)
+  // 	history.push("/login")
+  // }
 
   return (
-    <Security {...config} onAuthRequired={authHandler}>
-      <Switch>
-        <Route path="/login" component={LoginPage} />
-        <Route path="/implicit/callback" component={LoginCallback} />
-        <Route path="/landing" component={LandingPage} />
-        {/* any of the routes you need secured should be registered as SecureRoutes */}
-        <SecureRoute
-          path="/"
-          exact
-          component={() => <HomePage LoadingComponent={LoadingComponent} />}
-        />
-        <SecureRoute path="/example-list" component={ExampleListPage} />
-        <SecureRoute path="/profile-list" component={ProfileListPage} />
-        <SecureRoute path="/datavis" component={ExampleDataViz} />
-        <Route component={NotFoundPage} />
-      </Switch>
-    </Security>
+    // ! Commented out <Security> because it's an Okta component -- we're using Google Auth
+    // <Security {...config} onAuthRequired={authHandler}>  <-------------- OKTA --------------
+    <Switch>
+      <Route path="/login" component={LoginPage} />
+
+      {/* <Route path="/implicit/callback" component={LoginCallback} /> <-------------- OKTA -------------- */}
+      <Route path="/landing" component={LandingPage} />
+      {/* any of the routes you need secured should be registered as SecureRoutes */}
+      <PrivateRoute
+        path="/"
+        exact
+        component={() => <HomePage LoadingComponent={LoadingComponent} />}
+      />
+      <PrivateRoute path="/example-list" component={ExampleListPage} />
+      <PrivateRoute path="/profile-list" component={ProfileListPage} />
+      <PrivateRoute path="/datavis" component={ExampleDataViz} />
+      <Route component={NotFoundPage} />
+    </Switch>
+    // </Security>
   );
 }
