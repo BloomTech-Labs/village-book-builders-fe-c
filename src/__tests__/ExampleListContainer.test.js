723c993d6ff1bb5a113ef07d3c5e09c1570e6971
yirano <yirano@protonmail.ch>
2020-12-07 19:13:29 -0500

    Comment out all tests but keep as reference for later

--- src/__tests__/ExampleListContainer.test.js
+++ src/__tests__/ExampleListContainer.test.js
@@ -1,21 +1,21 @@
-import React from 'react';
-import { render, act, cleanup, waitForElement } from '@testing-library/react';
+// import React from 'react';
+// import { render, act, cleanup, waitForElement } from '@testing-library/react';
 
-import ExampleListContainer from '../components/pages/ExampleList/ExampleListContainer';
+// import ExampleListContainer from '../components/pages/ExampleList/ExampleListContainer';
 
-afterEach(() => {
-  cleanup();
-  jest.clearAllMocks();
-});
+// afterEach(() => {
+//   cleanup();
+//   jest.clearAllMocks();
+// });
 
-jest.mock('../api', () => ({
-  getExampleData: jest.fn(() => Promise.resolve([])),
-}));
+// jest.mock('../api', () => ({
+//   getExampleData: jest.fn(() => Promise.resolve([])),
+// }));
 
-describe('<ExampleListContainer /> test suite', () => {
-  test('container renders without crashing', async () => {
-    await act(async () => {
-      await render(<ExampleListContainer />);
-    });
-  });
-});
+// describe('<ExampleListContainer /> test suite', () => {
+//   test('container renders without crashing', async () => {
+//     await act(async () => {
+//       await render(<ExampleListContainer />);
+//     });
+//   });
+// });
