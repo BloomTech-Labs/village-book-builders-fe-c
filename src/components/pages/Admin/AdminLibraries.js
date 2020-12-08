093c6f284d99e8ab69a7c443d2a8a2d4c43414a4
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-08 12:43:22 -0800

    setup temp libary data to display in AdminLibraries. AdminDashboard was supposed to be removed in previous commit, added it here finally

--- src/components/pages/Admin/AdminLibraries.js
+++ src/components/pages/Admin/AdminLibraries.js
@@ -1,9 +1,53 @@
 import React from 'react';
+import { Button, Divider } from 'antd';
 
 export default function AdminLibraries() {
+  const mockLibraries = [
+    {
+      id: 1,
+      name: 'Okanoke Library',
+      description: 'really tall building on the left',
+      library_usage: 'this will be a paragraph',
+      notes: 'another optiona paragraph',
+      image: 'Url link here',
+    },
+    {
+      id: 2,
+      name: 'Merry Moving Library',
+      description: 'cute, pink bus',
+      library_usage: 'this will be a paragraph',
+      notes: 'another optiona paragraph',
+      image: 'Url link here',
+    },
+    {
+      id: 3,
+      name: 'First Fallon Library',
+      description: 'Metal bunker at edge of town',
+      library_usage: 'this will be a paragraph',
+      notes: 'another optiona paragraph',
+      image: 'Url link here',
+    },
+  ];
+
   return (
     <>
-      <h1>Here's your current list of libraries, Administrator.</h1>
+      <h1>Libraries, Admin View.</h1>
+      <Divider />
+      <Button>Add New Library</Button>
+      {mockLibraries.map(library => {
+        return (
+          <div>
+            <h2>{library.name}</h2>
+            <p>{library.description}</p>
+            {/* <p>{library.library_usage}</p>
+            <p>{library.notes}</p>
+            <p>{library.image}</p> */}
+            <Button>More Info</Button>
+            <Button>Edit</Button>
+            <Divider />
+          </div>
+        );
+      })}
     </>
   );
 }
--- src/components/pages/AdminDashboard/AdminDashboard.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-// import axios from 'axios';
-// import { connect } from 'react-redux';
-
-export default function AdminDashboard() {
-  // render() {
-  return 'admin Dashboard here';
-  // }
-}
