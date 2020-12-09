36abfe6044384f49e8689a9180dc0fb9c63f54c8
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-08 20:50:02 -0800

    added more-info and edit buttons to library section. Then added Modal for more-info button click and populated it. Edit buttons are wired to push to a new edit-libary page.

--- src/components/pages/Admin/AdminLibraries.js
+++ src/components/pages/Admin/AdminLibraries.js
@@ -1,5 +1,6 @@
-import React from 'react';
-import { Button, Divider } from 'antd';
+import React, { useState } from 'react';
+import { Button, Divider, Modal } from 'antd';
+import { useHistory } from 'react-router-dom';
 
 export default function AdminLibraries() {
   const mockLibraries = [
@@ -28,6 +29,25 @@ export default function AdminLibraries() {
       image: 'Url link here',
     },
   ];
+  const [showModal, setShowModal] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const { push } = useHistory();
+
+  function handleMoreInfo() {
+    // setLoading(true);
+    console.log('handleMoreInfo ran here');
+    setShowModal(true);
+    // setTimeout(() => {
+    //   this.setState({ loading: false, visible: false });
+    // }, 3000);
+  }
+
+  function handleEdit(libraryId) {
+    console.log('handle edit');
+    // better to pass the libary data here than do a second axios call at new page. Or set it to redux store here, so I can pull it out at new page. Decisions...
+    push(`/admin/library/edit/${libraryId}`);
+  }
 
   return (
     <>
@@ -36,14 +56,36 @@ export default function AdminLibraries() {
       <Button>Add New Library</Button>
       {mockLibraries.map(library => {
         return (
-          <div>
+          <div key={library.id}>
+            {/* TODO: make this a card instead & remove dividers or will that slow it down on low-end mobile devices?*/}
             <h2>{library.name}</h2>
             <p>{library.description}</p>
-            {/* <p>{library.library_usage}</p>
-            <p>{library.notes}</p>
-            <p>{library.image}</p> */}
-            <Button>More Info</Button>
-            <Button>Edit</Button>
+            <Button onClick={() => handleMoreInfo()}> More Info </Button>
+            <Button onClick={() => handleEdit(library.id)}> Edit </Button>
+            <Modal
+              visible={showModal}
+              title={library.name}
+              onOk={() => handleEdit(library.Id)}
+              onCancel={() => setShowModal(false)}
+              footer={[
+                <Button key="back" onClick={() => setShowModal(false)}>
+                  Done
+                </Button>,
+                <Button
+                  key="submit"
+                  type="primary"
+                  loading={loading}
+                  onClick={() => handleEdit(library.id)}
+                >
+                  Edit
+                </Button>,
+              ]}
+            >
+              <p>Description: {library.description}</p>
+              <p>Library Usage: {library.library_usage}</p>
+              <p>Notes: {library.notes}</p>
+              <p>Image: {library.image}</p>
+            </Modal>
             <Divider />
           </div>
         );
