8151d6ce432fad6d34c91e588ff2cc299df0c47c
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-16 14:59:29 -0800

    minor code cleanup

--- src/components/pages/Admin/AdminDashboard.js
+++ src/components/pages/Admin/AdminDashboard.js
@@ -1,21 +1,18 @@
 import React from 'react';
 import { Button } from 'antd';
-import { useHistory, Link, Route, Switch } from 'react-router-dom';
+import { Link, Route, Switch } from 'react-router-dom';
 import Libraries from './Libraries';
 import EditLibrary from './EditLibrary';
 import './AdminDashboard.css';
 
 export default function AdminDashboard() {
-  const { push } = useHistory();
-
   return (
     <div className="admin-dashboard-container">
       <h1>Welcome Administrator to your personal dashboard</h1>
-      {/* TODO: pushing to a new route works for mobile first, but may need to be modified for better browser designs */}
       <Link to="/admin/libraries">
         <Button>Libraries </Button>
       </Link>
-      <Button> Schools </Button>
+      <Button> Schools (Future release) </Button>
       <Button> xxxxxx </Button>
 
       <Switch>
