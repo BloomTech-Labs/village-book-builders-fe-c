6da19659744fe1745582cf96933429d3f91fb13e
jidelson <58210030+jidelson@users.noreply.github.com>
2020-12-17 15:04:54 -0800

    added headmaster profile

---
+++ src/components/pages/Headmaster/HeadmasterProfile/ProfileForm.js
@@ -0,0 +1,270 @@
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
+import { useParams, useHistory, Link } from 'react-router-dom';
+import axios from 'axios';
+
+import { Form, Input, DatePicker, Space, Checkbox } from 'antd';
+import moment from 'moment';
+
+import { editHeadmasterProfile } from '../../../../state/actions';
+import {
+  layout,
+  FormContainer,
+  tailLayout,
+  Required,
+} from '../../../common/FormStyle';
+import Button from '../../../common/Button';
+
+const baseURL = 'https://cors-anywhere.herokuapp.com/http://54.158.134.245/api';
+
+const initialState = {
+  first_name: '',
+  gender: {
+    male: false,
+    female: false,
+    other: false,
+  },
+  address: '',
+  bio: '',
+  communication_app: '',
+  dob: '',
+  general_availability: '',
+  goals_mentor_program: '',
+  goals_personal: '',
+  goals_school_community: '',
+  mentor_advisor_point_of_contact: '',
+  phone_number: '',
+  photo_url: '',
+  programId: '',
+  registration_status: '',
+  second_name: '',
+  time_zone: '',
+  villageId: '',
+};
+
+function onChange(checkedValues) {
+  console.log('checked = ', checkedValues);
+}
+const genderOptions = ['Male', 'Female', 'Other'];
+
+const dateFormat = 'MM/DD/YYYY';
+const dateFormatList = ['DD/MM/YYYY', 'DD/MM/YY'];
+
+const ProfileForm = props => {
+  const [formData, setFormData] = useState(initialState);
+  const pathname = useHistory().location.pathname;
+  const params = useParams().id;
+  const [form] = Form.useForm();
+
+  useEffect(() => {
+    axios // ! This should later become available through axiosWithAuth() only once we figure out the Auth with Stakeholder's backend
+      .get(`${baseURL}/headmaster/1`)
+      .then(res => {
+        form.setFieldsValue(res.data);
+        setFormData(res.data);
+      })
+      .catch(err => console.dir(err));
+  }, []);
+
+  const handleSubmit = async () => {
+    console.log(formData);
+    props.editHeadmasterProfile(params, formData);
+  };
+
+  const handleChange = e => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  return (
+    <FormContainer>
+      <Form.Item {...tailLayout}>
+        <Link to="/profile">Go Back to your Profile</Link>
+      </Form.Item>
+      <Form onFinish={handleSubmit} form={form} {...layout}>
+        <Form.Item
+          label="First Name"
+          name="first_name"
+          rules={[{ required: true, message: 'First Name is required.' }]}
+        >
+          <Input
+            type="text"
+            name="first_name"
+            defaultValue="Mr Headmaster" // Change this
+            value={formData.first_name}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Last Name"
+          name="second_name"
+          rules={[{ required: true, message: 'Last Name is required.' }]}
+        >
+          <Input
+            type="text"
+            name="last_name"
+            value={formData.last_name}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Space direction="vertical" size={12} {...tailLayout}>
+          <DatePicker
+            defaultValue={moment(`${formData.dob}`, dateFormatList[0])}
+            format={dateFormat}
+          />
+        </Space>
+
+        <Form.Item
+          label="Address"
+          name="address"
+          rules={[{ required: true, message: 'Address is required.' }]}
+        >
+          <Input
+            type="text"
+            name="address"
+            value={formData.address}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Phone Number"
+          name="phone_number"
+          rules={[{ required: true, message: 'Phone Number is required.' }]}
+        >
+          <Input
+            type="text"
+            name="phone_number"
+            value={formData.phone_number}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item label="Gender" name="gender" value={formData.gender}>
+          <Checkbox.Group
+            options={genderOptions}
+            defaultValue={[]}
+            onChange={onChange}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Bio"
+          name="bio"
+          rules={[{ required: true, message: 'Bio is required.' }]}
+        >
+          <Input
+            type="text"
+            name="bio"
+            value={formData.bio}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Communication App"
+          name="communication_app"
+          rules={[
+            { required: true, message: 'Communication app is required.' },
+          ]}
+        >
+          <Input
+            type="text"
+            name="communication_app"
+            value={formData.communication_app}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="General Availability"
+          name="general_availability"
+          rules={[
+            { required: true, message: 'General Availability is required' },
+          ]}
+        >
+          <Input
+            type="text"
+            name="general_availability"
+            value={formData.general_availability}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Mentor Program Goals"
+          name="goals_mentor_program"
+          rules={[
+            { required: true, message: 'Goals of mentor program is required.' },
+          ]}
+        >
+          <Input
+            type="text"
+            value={formData.goals_mentor_program}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Personal Goals"
+          name="goals_personal"
+          rules={[{ required: true, message: 'Personal goals are required.' }]}
+        >
+          <Input
+            type="text"
+            value={formData.goals_personal}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="School Community Goals"
+          name="goals_school_community"
+          rules={[
+            {
+              required: true,
+              message: 'Goals for schools community are required.',
+            },
+          ]}
+        >
+          <Input
+            type="text"
+            value={formData.goals_school_community}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item
+          label="Mentor Advisor Point of Contact"
+          name="mentor_advisor_point_of_contact"
+          rules={[
+            {
+              required: true,
+              message: 'Mentor advisor point of contact is required.',
+            },
+          ]}
+        >
+          <Input
+            type="text"
+            value={formData.mentor_advisor_point_of_contact}
+            onChange={e => handleChange(e)}
+          />
+        </Form.Item>
+
+        <Form.Item {...tailLayout}>
+          <Button
+            className="l2-btn btn"
+            htmlType="submit"
+            buttonText="Submit Village Edit"
+          />
+          <Required id="requiredMsg">
+            Fields with <span id="required">&#42;</span> are required.
+          </Required>
+        </Form.Item>
+      </Form>
+    </FormContainer>
+  );
+};
+
+export default connect(null, { editHeadmasterProfile })(ProfileForm);
