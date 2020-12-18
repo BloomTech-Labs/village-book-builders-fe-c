0411f389ad703460028df4305bf3b19d454f1679
Josh Glantz-Hucks <jahteo@gmail.com>
2020-12-17 20:08:39 -0800

    added login instructions

--- src/components/pages/Login/Login.js
+++ src/components/pages/Login/Login.js
@@ -68,6 +68,26 @@ const Login = props => {
           <Button buttonText="Log In" type="submit" />
         </Form.Item>
       </Form>
+      <h2>Temporary logins:</h2>
+      <p>Account info for testing:</p>
+      <p>"admin@admin.com" - "password"</p>
+      <p>"headmaster@headmaster.com" - "password"</p>
+      <p>more from server will be added soon</p>
+      <p>
+        Note to dev's: highly recommended to save these in your browser
+        autocomplete for sanity, until token validation is added to app.js
+      </p>
+      <p>
+        Note to dev's2: need to remove all page refreshes in code. Currently
+        causes user to require logging in again.
+      </p>
+      <h2>Temporary admin dashboard access:</h2>
+      <p>Instead of logging in, Add "/admin" after url.</p>
+      <p>
+        This will be wrapped into the login authorization once some refactoring
+        is done, and will match the headmaster dashboard/sidebar design. It's
+        seperate in the meantime for functionality testing.{' '}
+      </p>
     </FormContainer>
   );
 };
