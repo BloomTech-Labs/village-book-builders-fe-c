1de3370e1e46eb0bfd78123f54371a424bcc407e
ferror18 <ferror18@gmail.com>
2021-01-05 15:00:03 -0800

    .

--- src/components/pages/Headmaster/Mentees/Mentees.js
+++ src/components/pages/Headmaster/Mentees/Mentees.js
@@ -1,18 +1,56 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { axiosWithAuth } from '../../../../utils/axiosWithAuth';
-const Mentees = () => {
+import { List, Avatar } from 'antd';
+import { connect } from 'react-redux';
+import { checkToken, fetchMentees } from '../../../../state/actions/index';
+const Mentees = props => {
   useEffect(() => {
-    axiosWithAuth()
-      .get('mentee')
-      .then(res => console.log(res));
-    // console.log(axiosWithAuth);
+    props.fetchMentees();
   }, []);
+
   return (
-    <>
-      <h1>Mentor Pairings</h1>
-    </>
+    <div className="menteeContainer">
+      <h1 id="menteeTittle">Mentee Management</h1>
+      <div className="exploreWrapper">
+        <List
+          itemLayout="horizontal"
+          dataSource={props.mentees}
+          renderItem={item => (
+            <List.Item>
+              <List.Item.Meta
+                avatar={<Avatar src={item.mentee_picture} />}
+                title={
+                  <a href="https://ant.design">
+                    {item.first_name + ' ' + item.last_name}
+                  </a>
+                }
+                description={item.academic_description}
+              />
+            </List.Item>
+          )}
+        />
+        ,
+      </div>
+    </div>
   );
 };
 
-export default Mentees;
+const mapStateToProps = state => {
+  console.log(state.headmasterReducer.mentees);
+  let f20Mentees = [];
+  if (state.headmasterReducer.mentees.length < 1) {
+    for (let index = 0; index < 2; index++) {
+      f20Mentees.push(state.headmasterReducer.mentees[index]);
+    }
+  } else {
+    f20Mentees = state.headmasterReducer.mentees;
+  }
+
+  return {
+    mentees: f20Mentees,
+    userId: state.loginReducer.userId,
+    role: state.loginReducer.role,
+  };
+};
+
+export default connect(mapStateToProps, { checkToken, fetchMentees })(Mentees);
