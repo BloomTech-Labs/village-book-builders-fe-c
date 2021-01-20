96ccdddd32c4609bd5175d1647569802559e91ef
mike hill <michael-hill@lambdastudents.com>
2021-01-20 13:42:50 -0500

    added button for rerouting to student registration form and profile edit form

--- src/components/pages/Headmaster/Student/StudentRegistration.js
+++ src/components/pages/Headmaster/Student/StudentRegistration.js
@@ -1,33 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import { connect } from 'react-redux';
 import { fetchMentees } from '../../../../state/actions/index';
+import Moment from 'moment';
+import { useHistory } from 'react-router-dom';
 
 const StudentRegistration = props => {
   const [search, setSearch] = useState('');
-
+  const history = useHistory();
   const { fetchMentees } = props;
-  useEffect(() => {
-    fetchMentees();
-  }, [fetchMentees]);
-  console.log(props.mentees);
 
   const filteredStudents = props.mentees.filter(student => {
-    return student.last_name.includes(search);
+    return student.last_name.toLowerCase().includes(search);
   });
 
-  const onSubmit = () => {
-    filteredStudents();
+  const onSubmit = e => {
+    e.preventDefault();
+    fetchMentees();
+    setSearch('');
   };
 
   const onChange = e => {
     setSearch(e.target.value);
   };
+
   return (
     <div>
       <form onSubmit={onSubmit}>
         <label>
           Last Name
-          <input type="text" placeholder="Last Name" onChange={onChange} />
+          <input
+            type="text"
+            placeholder="Last Name"
+            value={search}
+            onChange={onChange}
+          />
           <input type="submit" />
         </label>
         <label>
@@ -37,19 +43,37 @@ const StudentRegistration = props => {
         </label>
       </form>
       <div>
-        {props.isLoading
-          ? '...Loading'
-          : filteredStudents.map(student => (
-              <div>
-                <h2>{student.mentee_picture}</h2>
-                <h2>
-                  {student.first_name} {student.last_name}
-                </h2>
-                <h2>{student.gender}</h2>
-                <h2>{student.primary_language}</h2>
-                <h2>{student.dob}</h2>
-              </div>
-            ))}
+        {filteredStudents.length === 0 ? (
+          <div>
+            <h2>This student is not registered.</h2>
+            <button
+              onClick={() => {
+                history.push('');
+              }}
+            >
+              Register Student
+            </button>
+          </div>
+        ) : (
+          filteredStudents.map(student => (
+            <div>
+              {/* this should all be styled in grid for easy viewing and we should consider importing cards as a styled component */}
+              <img
+                src={student.mentee_picture}
+                alt="somethings here"
+                style={{ borderRadius: '50%', width: '200px', height: '200px' }}
+              />{' '}
+              {/* temp styling */}
+              <h2>
+                {student.first_name} {student.last_name}
+              </h2>
+              <h3>Date Of Birth:{Moment(student.dob).format('DD-MM-YYYY')}</h3>
+              <h3>Gender:{student.gender}</h3>
+              <h3>Primary Language:{student.primary_language}</h3>
+              <button>Update</button>
+            </div>
+          ))
+        )}
       </div>
     </div>
   );
