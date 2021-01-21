33a23b7bfbce2f13107476934b8af5a8e93618dd
Mary Baker <meb601@gmail.com>
2021-01-21 18:26:17 -0500

    update StudentProfileForm component with boilerplate

---
+++ src/components/pages/Headmaster/Student/StudentProfileForm.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const StudentProfileForm = () => {
+  return <div></div>;
+};
+
+export default StudentProfileForm;
--- src/components/pages/Headmaster/Student/TeacherProfileForm.js
+++
@@ -1,7 +0,0 @@
-import React from 'react';
-
-const TeacherProfileForm = () => {
-  return <div></div>;
-};
-
-export default TeacherProfileForm;
