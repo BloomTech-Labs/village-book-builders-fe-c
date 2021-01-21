8b3740ae542be232706f23d9fb14395c6ddb4ac2
Mary Baker <meb601@gmail.com>
2021-01-21 17:33:00 -0500

    add ProgramDashboard with skeleton code and ProgramProfile and ProgramProfileForm boilerplate

---
+++ src/components/pages/Program/Drawer.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+function ProgramNav() {
+  return (
+    <div>
+      <Link to="/student-registration">Student Registration</Link>
+    </div>
+  );
+}
+
+export default ProgramNav;
