616d8553bbb93f6a5d4ce94da102bf909eab973a
Mary Baker <meb601@gmail.com>
2021-01-25 20:16:57 -0500

    render headmaster name in headmaster dashboard home

--- src/components/pages/Headmaster/HeadmasterHome.js
+++ src/components/pages/Headmaster/HeadmasterHome.js
@@ -1,11 +1,33 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchHeadmasterProfile } from '../../../state/actions';
 
-function HeadmasterHome() {
+const HeadmasterHome = props => {
+  const { profile } = props;
+
+  useEffect(() => {
+    props.fetchHeadmasterProfile(1); // change this later with login
+  }, []);
+  console.log(profile);
+
   return (
     <div>
-      <h1>Headmaster Home</h1>
+      <h1>
+        Hello,{' '}
+        <span
+          style={{ color: '#6AC66B' }}
+        >{`Headmaster ${profile.last_name}`}</span>
+      </h1>
     </div>
   );
-}
+};
 
-export default HeadmasterHome;
+const mapStateToProps = state => {
+  return {
+    profile: state.headmasterReducer.headmasterProfile,
+  };
+};
+
+export default connect(mapStateToProps, { fetchHeadmasterProfile })(
+  HeadmasterHome
+);
