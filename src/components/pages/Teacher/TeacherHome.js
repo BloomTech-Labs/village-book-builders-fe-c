fecf5a0306c36c21fb9d135e91c4d2e8549686d5
Mary Baker <meb601@gmail.com>
2021-01-25 20:22:10 -0500

    render teacher name in teacher dashboard home

--- src/components/pages/Teacher/TeacherHome.js
+++ src/components/pages/Teacher/TeacherHome.js
@@ -1,11 +1,31 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchTeacherProfile } from '../../../state/actions';
 
-function TeacherHome() {
+const TeacherHome = props => {
+  const { profile } = props;
+
+  useEffect(() => {
+    props.fetchTeacherProfile(1); // change this later with login
+  }, []);
+  console.log(profile);
+
   return (
     <div>
-      <h1>Teacher Home</h1>
+      <h1>
+        Hello,{' '}
+        <span
+          style={{ color: '#6AC66B' }}
+        >{`${profile.first_name} ${profile.last_name}`}</span>
+      </h1>
     </div>
   );
-}
+};
 
-export default TeacherHome;
+const mapStateToProps = state => {
+  return {
+    profile: state.teacherReducer.teacherProfile,
+  };
+};
+
+export default connect(mapStateToProps, { fetchTeacherProfile })(TeacherHome);
