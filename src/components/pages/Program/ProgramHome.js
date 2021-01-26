f62cda5243efa2dc1ab9cd057d39256cc3f6a575
Mary Baker <meb601@gmail.com>
2021-01-25 20:19:40 -0500

    render program name in program dashboard home

--- src/components/pages/Program/ProgramHome.js
+++ src/components/pages/Program/ProgramHome.js
@@ -1,11 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import { connect } from 'react-redux';
+import { fetchProgramProfile } from '../../../state/actions';
 
-function ProgramHome() {
+const ProgramHome = props => {
+  const { profile } = props;
+
+  useEffect(() => {
+    props.fetchProgramProfile(1); // change this later with login
+  }, []);
+  console.log(profile);
+
   return (
     <div>
-      <h1>Program Home</h1>
+      <h1>
+        Hello, <span style={{ color: '#6AC66B' }}>{`${profile.name}`}</span>
+      </h1>
     </div>
   );
-}
+};
 
-export default ProgramHome;
+const mapStateToProps = state => {
+  return {
+    profile: state.programReducer.programProfile,
+  };
+};
+
+export default connect(mapStateToProps, { fetchProgramProfile })(ProgramHome);
