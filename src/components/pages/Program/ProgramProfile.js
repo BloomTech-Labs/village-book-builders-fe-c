e7fa4ec4295fd93e6db028586135ff4d78bd5788
Mary Baker <meb601@gmail.com>
2021-02-01 11:27:07 -0500

    Remove legacy styling from program profile

--- src/components/pages/Program/ProgramProfile.js
+++ src/components/pages/Program/ProgramProfile.js
@@ -1,37 +1,27 @@
 import React, { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { ThemeProvider } from 'styled-components';
 import { fetchProgramProfile } from '../../../state/actions';
-import { Profile, Label } from '../../common/ProfileStyle';
-import { Button, ComponentTitle } from '../../common';
+import { Button } from 'antd';
 
 const ProgramProfile = ({ profile, fetchProgramProfile }) => {
   useEffect(() => {
     fetchProgramProfile(0); // change this later with login
   }, [fetchProgramProfile]);
   // console.log(profile);
+
   return (
-    <Profile>
-      <ComponentTitle titleText="Profile" />
-
-      <Label>Program Name:</Label>
-      <p>{`${profile.name}`}</p>
-
-      <Label>Location:</Label>
-      <p>{profile.location}</p>
-
-      <Label>Library ID:</Label>
-      <p>{profile.libraryId}</p>
-
+    <div>
+      <h1>Profile</h1>
+      <p>Program Name: {`${profile.name}`}</p>
+      <p>Location: {profile.location}</p>
+      <p>Library ID: {profile.libraryId}</p>
       <div>
         <Link to={`/profile/edit/${profile.id}`}>
-          <ThemeProvider theme={{ color: '#6ac66b' }}>
-            <Button buttonText="Edit Your Profile" />
-          </ThemeProvider>
+          <Button>Edit</Button>
         </Link>
       </div>
-    </Profile>
+    </div>
   );
 };
 
