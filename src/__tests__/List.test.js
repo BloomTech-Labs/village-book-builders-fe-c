dad922679cadff5e9e6a43c6bc3f3668146c21ba
Mary Baker <meb601@gmail.com>
2021-02-01 19:43:01 -0500

    Comment out irrelevant tests for now

--- src/__tests__/List.test.js
+++ src/__tests__/List.test.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import { render, cleanup, act } from '@testing-library/react';
+// import React from 'react';
+// import { render, cleanup, act } from '@testing-library/react';
 
-import { List } from '../components/common';
+// import { List } from '../components/common';
 
-afterEach(() => {
-  cleanup();
-});
+// afterEach(() => {
+//   cleanup();
+// });
 
-const getItemsData = jest.fn(() =>
-  Promise.resolve([{ id: 1 }, { id: 2 }, { id: 3 }])
-);
-const RenderItems = jest.fn(props =>
-  props.data.map(item => <li key={item.id}>Item</li>)
-);
+// const getItemsData = jest.fn(() =>
+//   Promise.resolve([{ id: 1 }, { id: 2 }, { id: 3 }])
+// );
+// const RenderItems = jest.fn(props =>
+//   props.data.map(item => <li key={item.id}>Item</li>)
+// );
