d08ab529cdabe54c65c91a17d2c0f5732aa5a0d0
Mary Baker <meb601@gmail.com>
2021-02-01 19:34:21 -0500

    Remove loading test since we are no longer using the loading component

--- src/__tests__/List.test.js
+++ src/__tests__/List.test.js
@@ -13,39 +13,3 @@ const getItemsData = jest.fn(() =>
 const RenderItems = jest.fn(props =>
   props.data.map(item => <li key={item.id}>Item</li>)
 );
-
-describe('<List /> test suite', () => {
-  test("renders 'loading' component initially", async () => {
-    let rendered;
-
-    act(() => {
-      rendered = render(
-        <List
-          // return an unresolved promise to test initial component state
-          getItemsData={jest.fn(() => new Promise(() => {}))}
-          LoadingComponent={() => <div>Loading...</div>}
-          RenderItems={RenderItems}
-        />
-      );
-    });
-
-    expect(rendered.getByText('Loading...').textContent).toBe('Loading...');
-  });
-  test('renders item data', async () => {
-    let rendered;
-
-    await act(async () => {
-      rendered = await render(
-        <List
-          getItemsData={getItemsData}
-          LoadingComponent={() => <div>Loading...</div>}
-          RenderItems={RenderItems}
-        />
-      );
-    });
-
-    // We expect 3 child elements to render corresponding to the 3 objects
-    // in our mock dataset
-    expect(rendered.container.children).toHaveLength(3);
-  });
-});
