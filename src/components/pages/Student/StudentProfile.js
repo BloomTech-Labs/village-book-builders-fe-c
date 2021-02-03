09bb8f476b0fbb365626049c33ff68bcf4a70e5a
Mary Baker <meb601@gmail.com>
2021-02-03 14:42:02 -0500

    Still experiencing issues with justify-content center in student profile, so adding temp center alignement

--- src/components/pages/Student/StudentProfile.js
+++ src/components/pages/Student/StudentProfile.js
@@ -4,53 +4,53 @@ import { connect } from 'react-redux';
 import { Button } from 'antd';
 import { fetchMenteeProfile } from '../../../state/actions';
 
-const StudentProfile = ({ fetchMenteeProfile, profile, isLoading }) => {
+const StudentProfile = ({ fetchMenteeProfile, profile }) => {
   useEffect(() => {
     fetchMenteeProfile(1); // change this later with login
-  }, []);
+  }, [fetchMenteeProfile]);
   console.log('inside the student profile', profile);
 
   return (
     <div>
-      {isLoading ? (
-        '...loading'
-      ) : (
-        <div className="form-container">
-          <div>
-            <h1 className="page-title">Profile</h1>
-            <img
-              className="profile-pic"
-              src={profile.mentee_picture}
-              alt="student profile"
-            />
-            <div className="profile-item-title">Name</div>
-            <div className="profile-item">
-              {profile.first_name} {profile.last_name}
-            </div>
-            <div className="profile-item-title">Gender</div>
-            <div className="profile-item">{profile.gender}</div>
-            <div className="profile-item-title">Email</div>
-            <div className="profile-item">{profile.email}</div>
-            <div className="profile-item-title">English level</div>
-            <div className="profile-item">{profile.english_lvl}</div>
-            <div className="profile-item-title">Math level</div>
-            <div className="profile-item">{profile.math_lvl}</div>
-            <div className="profile-item-title">Reading level</div>
-            <div className="profile-item">{profile.reading_lvl}</div>
-            <div className="profile-item-title">School level</div>
-            <div className="profile-item">{profile.school_lvl}</div>
-            <div className="profile-item-title">Academic description</div>
-            <div className="profile-item">{profile.academic_description}</div>
-            <div className="profile-item-title">Support needed</div>
-            <div className="profile-item">{profile.support_needed}</div>
-            <div>
-              <Link to={`/profile/edit/${profile.id}`}>
-                <Button style={{ margin: '.5rem 0' }}>Edit</Button>
-              </Link>
-            </div>
-          </div>
+      <div
+        style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+        }}
+      >
+        <h1 className="page-title">Profile</h1>
+        <img
+          className="profile-pic"
+          src={profile.mentee_picture}
+          alt="student profile"
+        />
+        <div className="profile-item-title">Name</div>
+        <div className="profile-item">
+          {profile.first_name} {profile.last_name}
         </div>
-      )}
+        <div className="profile-item-title">Gender</div>
+        <div className="profile-item">{profile.gender}</div>
+        <div className="profile-item-title">Email</div>
+        <div className="profile-item">{profile.email}</div>
+        <div className="profile-item-title">English level</div>
+        <div className="profile-item">{profile.english_lvl}</div>
+        <div className="profile-item-title">Math level</div>
+        <div className="profile-item">{profile.math_lvl}</div>
+        <div className="profile-item-title">Reading level</div>
+        <div className="profile-item">{profile.reading_lvl}</div>
+        <div className="profile-item-title">School level</div>
+        <div className="profile-item">{profile.school_lvl}</div>
+        <div className="profile-item-title">Academic description</div>
+        <div className="profile-item">{profile.academic_description}</div>
+        <div className="profile-item-title">Support needed</div>
+        <div className="profile-item">{profile.support_needed}</div>
+        <div>
+          <Link to={`/profile/edit/${profile.id}`}>
+            <Button style={{ margin: '.5rem 0' }}>Edit</Button>
+          </Link>
+        </div>
+      </div>
     </div>
   );
 };
@@ -58,7 +58,6 @@ const StudentProfile = ({ fetchMenteeProfile, profile, isLoading }) => {
 const mapStateToProps = state => {
   return {
     profile: state.menteeReducer.menteeProfile,
-    isLoading: state.menteeReducer.isLoading,
   };
 };
 export default connect(mapStateToProps, { fetchMenteeProfile })(StudentProfile);
