3098a24613812e59604ff12010a2abc189eec6f2
Mary Baker <meb601@gmail.com>
2021-02-03 14:24:40 -0500

    Update school and village edit forms

--- src/components/pages/School/SchoolForm.js
+++ src/components/pages/School/SchoolForm.js
@@ -49,102 +49,101 @@ const SchoolForm = props => {
   };
 
   return (
-    <div>
-      <Form.Item>
-        <Link to="/school-village">Go Back</Link>
-      </Form.Item>
-
-      <Form onFinish={handleSubmit} form={form}>
-        <Form.Item
-          label="Headmaster"
-          name="headmaster"
-          rules={[{ required: true, message: 'Headmaster name is required.' }]}
-        >
-          <Input
-            type="text"
-            name="headmaster"
-            value={formData.headmaster}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="School Description"
-          name="school_description"
-          rules={[
-            { required: true, message: 'School description is required.' },
-          ]}
-        >
-          <Input
-            type="text"
-            name="school_description"
-            value={formData.school_description}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item label="School Goals Description" name="school_goals">
-          <Input
-            type="text"
-            name="school_goals"
-            value={formData.school_goals}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item label="School Needs Description" name="school_needs">
-          <Input
-            type="text"
-            name="school_needs"
-            value={formData.school_needs}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Students Currently Enrolled"
-          name="count_students_currently_enrolled"
-          rules={[{ required: true, message: 'Student count is required.' }]}
-        >
-          <Input
-            type="text"
-            name="count_students_currently_enrolled"
-            value={formData.count_students_currently_enrolled}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item
-          label="Teachers Currently Teaching"
-          name="count_teachers"
-          rules={[{ required: true, message: 'Teacher count is required.' }]}
-        >
-          <Input
-            type="text"
-            name="count_teachers"
-            value={formData.count_teachers}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
-        <Form.Item label="Notes" name="notes">
-          <Input.TextArea
-            name="notes"
-            value={formData.notes}
-            onChange={e => handleChange(e)}
-          />
-        </Form.Item>
-
+    <div className="form-container">
+      <Form>
+        <h1 className="page-title">Edit School</h1>
         <Form.Item>
-          <Button
-            className="l2-btn btn"
-            htmlType="submit"
-            buttonText="Submit School Edit"
-          />
-          <div>
-            Fields with <span id="required">&#42;</span> are required.
-          </div>
+          <Link to="/school-village">Go Back</Link>
         </Form.Item>
+
+        <Form onFinish={handleSubmit} form={form}>
+          <Form.Item
+            label="Headmaster"
+            name="headmaster"
+            rules={[
+              { required: true, message: 'Headmaster name is required.' },
+            ]}
+          >
+            <Input
+              type="text"
+              name="headmaster"
+              value={formData.headmaster}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="School Description"
+            name="school_description"
+            rules={[
+              { required: true, message: 'School description is required.' },
+            ]}
+          >
+            <Input
+              type="text"
+              name="school_description"
+              value={formData.school_description}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item label="School Goals Description" name="school_goals">
+            <Input
+              type="text"
+              name="school_goals"
+              value={formData.school_goals}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item label="School Needs Description" name="school_needs">
+            <Input
+              type="text"
+              name="school_needs"
+              value={formData.school_needs}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Students Currently Enrolled"
+            name="count_students_currently_enrolled"
+            rules={[{ required: true, message: 'Student count is required.' }]}
+          >
+            <Input
+              type="text"
+              name="count_students_currently_enrolled"
+              value={formData.count_students_currently_enrolled}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item
+            label="Teachers Currently Teaching"
+            name="count_teachers"
+            rules={[{ required: true, message: 'Teacher count is required.' }]}
+          >
+            <Input
+              type="text"
+              name="count_teachers"
+              value={formData.count_teachers}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item label="Notes" name="notes">
+            <Input.TextArea
+              name="notes"
+              value={formData.notes}
+              onChange={e => handleChange(e)}
+            />
+          </Form.Item>
+
+          <Form.Item>
+            <p>Fields with * are required.</p>
+            <Button>Submit</Button>
+          </Form.Item>
+        </Form>
       </Form>
     </div>
   );
