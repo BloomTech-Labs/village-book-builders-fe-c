860755a4b40b72c2d4413deac6088c22e4354b93
Michelle Hey <48541701+HeyMichelle@users.noreply.github.com>
2021-03-22 22:29:35 -0400

    Feature/Modal: showing person info for mentors and mentees (#28)
    * refactor: changes made to style modal and correctly pass props
    
    * feat: modal opens onclick of mentor, to show profile info
    
    * feat: information now renders for mentors or mentees on click of name in mentor or mentee list
    
    * merging issues
    
    * fix: merge resolution

---
+++ src/components/pages/Headmaster/MentorMenteeMatching/PersonInfoModal.js
@@ -0,0 +1,61 @@
+import React from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { hideModal } from '../../../../state/actions';
+import { Modal, Button } from 'antd';
+import 'antd/dist/antd.css';
+
+const PersonInfoModal = props => {
+  const personInfoModalVisible = useSelector(
+    state => state.calendarReducer.personInfoModalVisible
+  );
+  const isLoading = useSelector(state => state.calendarReducer.isLoading);
+  const PersonInfoModal = useSelector(
+    state => state.calendarReducer.PersonInfoModal
+  );
+
+  const dispatch = useDispatch();
+
+  const handleOk = () => {
+    console.log('Should be rendering person info');
+  };
+
+  const handleCancel = () => {
+    dispatch(hideModal());
+  };
+
+  const seeDetailedProfile = () => {
+    console.log('Should eventually link to user profiles');
+  };
+
+  return (
+    <>
+      <Modal
+        visible={personInfoModalVisible}
+        title={`${PersonInfoModal.first_name} ${PersonInfoModal.last_name}`}
+        onOk={handleOk}
+        onCancel={handleCancel}
+        className={props.className}
+        footer={[
+          <Button onClick={seeDetailedProfile}>
+            See Detailed Profile Information
+          </Button>,
+        ]}
+      >
+        <p>
+          <b>DOB</b>: {PersonInfoModal.dob}{' '}
+        </p>
+        <p>
+          <b>Gender</b>: {PersonInfoModal.gender}{' '}
+        </p>
+        <p>
+          <b>Primary Language</b>:{PersonInfoModal.primary_language}{' '}
+        </p>
+        <p>
+          <b>Academic Description</b>: {PersonInfoModal.academic_description}{' '}
+        </p>
+      </Modal>
+    </>
+  );
+};
+
+export default PersonInfoModal;
